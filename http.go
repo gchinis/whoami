de8c6525f2d3c9a7487cb76eaaa72490c50f8a92
Georgios Chinis <gchinis@thoughtworks.com>
2017-03-26 11:28:30 +0200

    Add delay in responses, to explore the behaviour of rolling updates

--- http.go
+++ http.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "net/http"
   "log"
+  "time"
 )
 
 func main() {
@@ -16,8 +17,9 @@ func main() {
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        time.Sleep(10 * time.Second)
         fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
+        fmt.Fprintf(w, "I'm %s\n", hostname)
     })
 
 
